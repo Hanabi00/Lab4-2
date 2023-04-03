3910d9ae675d1fd41349f260a0948c7488512792
Hanabi00 <coody680@gmail.com>
2023-04-03 13:26:00 -0400

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ANazhatOOP.Models;
+
+namespace ANazhatOOP.Controllers
+{
+    public class UsersController : Controller
+    {
+        UsersContext uc = new();
+        public IActionResult ANView()
+        {
+            return View(uc.AnazhatTables.ToList());
+        }
+    }
+}
